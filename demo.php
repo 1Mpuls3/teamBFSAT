e8a8fbf2791abea6ce97a83c630919620a78a65c
unknown <vod@.(none)>
2015-05-10 21:40:23 +0200

    test

--- demo.php
+++ demo.php
@@ -19,7 +19,7 @@ while ($row = mysql_fetch_assoc($query)) {
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title><?php echo "hello world"; ?></title>
+		<title>AJAX-ed To Do List With PHP, jQuery &amp; MySQL | Tutorialzine demo</title>
 
 		<!-- Including the jQuery UI Human Theme -->
 		<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.0/themes/humanity/jquery-ui.css" type="text/css" media="all" />
